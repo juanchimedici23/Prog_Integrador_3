5e540cdf807b4d602cc0c2f315ed0f88bdafa422
juanchimedici23 <jmedici@udesa.edu.ar>
2023-09-11 15:21:35 -0300

    App

--- proyecto_integrador/src/App.js
+++ proyecto_integrador/src/App.js
@@ -2,22 +2,11 @@ import React from "react";
 import Header from './components/Header/Header'
 import Footer from "./components/Footer/Footer";
 import Home from "./screens/Home/Home";
-import { Link, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <React.Fragment>
     <Header/>
-    <Switch>
-
-      <Route path="" exact={true} component={}/>
-      <Route path="" exact={true} component={}/>
-      <Route path="" exact={true} component={}/>
-      <Route path="" exact={true} component={}/>
-      <Route path="" exact={true} component={}/>
-      <Route path="" exact={true} component={}/>
-
-    </Switch>
     <Footer/>
     </React.Fragment>
     );
