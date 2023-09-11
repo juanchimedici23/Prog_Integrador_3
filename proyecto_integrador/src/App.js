2b26f2a4564f6b6687ff6d60d9bf201ddab18b89
juanchimedici23 <jmedici@udesa.edu.ar>
2023-09-11 12:12:18 -0300

    header y Footer con css incluido

--- proyecto_integrador/src/App.js
+++ proyecto_integrador/src/App.js
@@ -1,7 +1,15 @@
+import React from "react";
+import Header from './components/Header/Header'
+import Footer from "./components/Footer/Footer";
+import Home from "./screens/Home/Home"
 function App() {
   return (
-    <p>React</p>
-  );
+    <React.Fragment>
+    <Header/>
+    
+    <Footer/>
+    </React.Fragment>
+    );
 }
 
 export default App;
